67473ccb0362ebc21a778644b720d60e74cc42f8
Mark <glitch7788@gmail.com>
2020-02-04 20:01:42 +0000

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ServerComponent} from './server/server.component';
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { AlertErrorComponent } from './alert-error/alert-error.component';
+import { AlertSuccessComponent } from './alert-success/alert-success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    AlertErrorComponent,
+    AlertSuccessComponent
   ],
   imports: [
     BrowserModule
